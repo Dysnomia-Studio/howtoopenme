4edc2a8db589b149200fbf91baac6e65b1666424
Elanis <elanis@hotmail.com>
2018-05-16 19:31:58 +0200

    Fix js

--- app/views/menu.php
+++ app/views/menu.php
@@ -13,9 +13,9 @@
 	        <div id="language-list"></div>
 	        <input type="text" id="selected-language" name="selected-language">
 	    </form>
-		<script type="text/javascript" src="js/iconselect/control/iconselect.js"></script>
-		<script type="text/javascript" src="js/iconselect/iscroll.js"></script>
-		<script type="text/javascript" src="js/language-list.js"></script>
+		<script type="text/javascript" src="/js/iconselect/control/iconselect.js"></script>
+		<script type="text/javascript" src="/js/iconselect/iscroll.js"></script>
+		<script type="text/javascript" src="/js/language-list.js"></script>
 	    <?php 
 	    if($lang->getLanguage() == 'en') { ?>
 	    <script type="text/javascript">
