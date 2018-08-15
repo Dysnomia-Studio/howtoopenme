3d333aebffc34c5759a0fac682c0c711f5e3743b
Elanis <elanis@hotmail.com>
2018-08-15 09:13:50 +0200

    Fix for subdirs in stats

--- app/controllers/stats.php
+++ app/controllers/stats.php
@@ -1,9 +1,11 @@
 <?php
 // Stats
-if(isset($_GET['ext']) && !empty($_GET['ext'])) { // Ext
-	$em = new ExtensionsManager();
-	$em->incViewCount($s);
-} elseif(isset($_GET['soft']) && !empty($_GET['soft'])) { // Soft
-	$sm = new SoftwaresManager();
-	$sm->incViewCount($s);
+if(count(explode("/", $s)) == 1) { // Disable subidirs
+	if(isset($_GET['ext']) && !empty($_GET['ext'])) { // Ext
+		$em = new ExtensionsManager();
+		$em->incViewCount($s);
+	} elseif(isset($_GET['soft']) && !empty($_GET['soft'])) { // Soft
+		$sm = new SoftwaresManager();
+		$sm->incViewCount($s);
+	}
 }
