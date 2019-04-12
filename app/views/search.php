26521d1103a2616bd1e4e95dbdf9eb7da13308f9
Elanis <elanis@hotmail.com>
2019-04-12 18:33:51 +0200

    Added adblocker message
    + Fixed display in search when a category have 0 entries

--- app/views/search.php
+++ app/views/search.php
@@ -3,29 +3,49 @@
 
 </section>
 <section class="corps corps-ad">
-	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-	<!-- Howtoopen.me - Responsive -->
-	<ins class="adsbygoogle"
-		 style="display:block"
-		 data-ad-client="***REMOVED***"
-		 data-ad-slot="***REMOVED***"
-		 data-ad-format="auto"></ins>
-	<script>
-	(adsbygoogle = window.adsbygoogle || []).push({});
-	</script>
+	<div class="beforeResult">
+		<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+		<!-- Howtoopen.me - Responsive -->
+		<ins class="adsbygoogle"
+			 style="display:block"
+			 data-ad-client="***REMOVED***"
+			 data-ad-slot="***REMOVED***"
+			 data-ad-format="auto"></ins>
+		<script>
+		(adsbygoogle = window.adsbygoogle || []).push({});
+		</script>
+
+		<div id="disclaimerMessage" class="disclaimerMessage">
+			Hey you !<br/>
+			Can you disable you adblocker please ?<br/>
+			This is our only way to finance this website, we promise we won't display more than one ad per page :)<br/>
+			Thanks and have fun with our tool !
+		</div>
+
+		<script type="text/javascript">
+			window.addEventListener('load', () => {
+				if(window.getComputedStyle(document.getElementsByClassName('beforeResult')[0]).height.charAt(0) === '0') {
+					document.getElementById('disclaimerMessage').style.display = 'block';
+				}
+			});
+		</script>
+	</div>
 </section>
 <section class="corps center-corps search-corps">
-	<h2><?= EXTENSIONS ?></h2>
-	<ul>
-	<?php
-		foreach ($extensions as $ext) {
-			$ext = json_decode(json_encode($ext), true);
+	<?php if(count($extensions) > 0) { ?>
+		<h2><?= EXTENSIONS ?></h2>
+		<ul>
+		<?php
+			foreach ($extensions as $ext) {
+				$ext = json_decode(json_encode($ext), true);
 
-			echo '<li><a href="/ext/'.$ext['ext'].'">'.$ext['ext'].'</a></li>';
-		}
-	?>
-	</ul>
+				echo '<li><a href="/ext/'.$ext['ext'].'">'.$ext['ext'].'</a></li>';
+			}
+		?>
+		</ul>
+	<?php } ?>
 
+	<?php if(count($softwares) > 0) { ?>
 	<h2><?= SOFTWARES ?></h2>
 	<ul>
 	<?php
@@ -41,16 +61,5 @@
 		}
 	?>
 	</ul>
-</section>
-<section class="corps corps-ad">
-	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-	<!-- Howtoopen.me - Responsive -->
-	<ins class="adsbygoogle"
-		 style="display:block"
-		 data-ad-client="***REMOVED***"
-		 data-ad-slot="***REMOVED***"
-		 data-ad-format="auto"></ins>
-	<script>
-	(adsbygoogle = window.adsbygoogle || []).push({});
-	</script>
+	<?php } ?>
 </section>
--- app/views/soft.php
+++ app/views/soft.php
@@ -8,16 +8,33 @@
 
 </section>
 <section class="corps corps-ad">
-	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-	<!-- Howtoopen.me - Responsive -->
-	<ins class="adsbygoogle"
-		 style="display:block"
-		 data-ad-client="***REMOVED***"
-		 data-ad-slot="***REMOVED***"
-		 data-ad-format="auto"></ins>
-	<script>
-	(adsbygoogle = window.adsbygoogle || []).push({});
-	</script>
+	<div class="beforeResult">
+		<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+		<!-- Howtoopen.me - Responsive -->
+		<ins class="adsbygoogle"
+			 style="display:block"
+			 data-ad-client="***REMOVED***"
+			 data-ad-slot="***REMOVED***"
+			 data-ad-format="auto"></ins>
+		<script>
+		(adsbygoogle = window.adsbygoogle || []).push({});
+		</script>
+
+		<div id="disclaimerMessage" class="disclaimerMessage">
+			Hey you !<br/>
+			Can you disable you adblocker please ?<br/>
+			This is our only way to finance this website, we promise we won't display more than one ad per page :)<br/>
+			Thanks and have fun with our tool !
+		</div>
+
+		<script type="text/javascript">
+			window.addEventListener('load', () => {
+				if(window.getComputedStyle(document.getElementsByClassName('beforeResult')[0]).height.charAt(0) === '0') {
+					document.getElementById('disclaimerMessage').style.display = 'block';
+				}
+			});
+		</script>
+	</div>
 </section>
 <section class="corps corps-view">
 
